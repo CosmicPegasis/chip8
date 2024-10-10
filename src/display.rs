576a3038b84a9d961c6755d4b3e802028b93ce98
Aviral Singh <aviralsingh@protonmail.com>
2024-10-11 00:22:33 +0530

    remove redundancy

--- src/display.rs
+++ src/display.rs
@@ -1,9 +1,14 @@
 use fltk::{prelude::*, *};
 use std::cell::RefCell;
 use std::rc::Rc;
+
+// TODO Need to figure out a way to only redraw the display a maximum of 60 times per second
+// TODO Need to only execute ~700 instructions per second
+
 pub struct EmuDisplay {
     inner: widget::Widget,
     pixel_mat: Rc<RefCell<[[bool; 64]; 32]>>,
+    key_pressed: Rc<RefCell<Option<u8>>>,
 }
 
 impl EmuDisplay {
@@ -18,7 +23,8 @@ impl EmuDisplay {
         let pixel_mat = [[false; 64]; 32];
         let pixel_mat = Rc::from(RefCell::from(pixel_mat));
         let draw_mat = pixel_mat.clone();
-        //       let handle_mat = pixel_mat.clone();
+        let key_pressed = Rc::new(RefCell::new(None));
+        let handle_key = key_pressed.clone();
         inner.draw(move |i| {
             let mat = draw_mat.borrow();
             for row in 0..31 {
@@ -48,9 +54,48 @@ impl EmuDisplay {
                 i.redraw();
                 true
             }
+            enums::Event::KeyDown => {
+                let key = app::event_key();
+                let key_char = key.to_char();
+                let chip8_key = match key_char {
+                    Some('1') => Some(0x1),
+                    Some('2') => Some(0x2),
+                    Some('3') => Some(0x3),
+                    Some('4') => Some(0xC),
+                    Some('q') => Some(0x4),
+                    Some('w') => Some(0x5),
+                    Some('e') => Some(0x6),
+                    Some('r') => Some(0xD),
+                    Some('a') => Some(0x7),
+                    Some('s') => Some(0x8),
+                    Some('d') => Some(0x9),
+                    Some('f') => Some(0xE),
+                    Some('z') => Some(0xA),
+                    Some('x') => Some(0x0),
+                    Some('c') => Some(0xB),
+                    Some('v') => Some(0xF),
+                    _ => None,
+                };
+                if let Some(k) = chip8_key {
+                    *handle_key.borrow_mut() = Some(k);
+                }
+                true
+            }
+            enums::Event::KeyUp => {
+                *handle_key.borrow_mut() = None;
+                true
+            }
             _ => false,
         });
-        Self { inner, pixel_mat }
+        Self {
+            inner,
+            pixel_mat,
+            key_pressed,
+        }
+    }
+
+    pub fn get_key_pressed(&self) -> Option<u8> {
+        *self.key_pressed.borrow()
     }
 }
 
